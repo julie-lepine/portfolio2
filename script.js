024afaeaf5919dd4f5833452b3e82ab1b6879830
Julie lepine <lepine.julie@live.fr>
2023-03-14 15:19:42 +0100

    header ok

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-// DROPDOWN MENU
-// When the user clicks on the button, toggle between hiding and showing the dropdown content 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+// transition au scroll
+const ratio = 0.1
+
+// objet qui définit l'ensemble des options de notre intersection observer
+var options = {
+    // élément qui servira de zone d'affichage et qui permettra de détecter si l'élément est visible ou non
+    root: null,
+    // marges sur les côtés de la zone d'affichage
+    rootMargin: '0px',
+    // à partir de quel moment le système d'intersection est détecté. 1 : entiereté de l'élément doit être visible
+    threshold: ratio
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
         }
-    }
+    })
 }
+
+const observer = new IntersectionObserver(handleIntersect, options)
+// observer l'élément avec la classe reveal
+document.querySelectorAll('.reveal').forEach(function (r) {
+    observer.observe(r)
+})
